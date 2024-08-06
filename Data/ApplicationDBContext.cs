d4fa3dfecb1e6eb50fbce80162682b2aea126a11
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-08-06 20:47:43 +0100

    feat: Entity Framework

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+    public DbSet<Stock> Stock { get; set; }
+    public DbSet<Comment> Comment { get; set; }
+}
