65d2e66a3d33237adc18680a49a6cb013471575c
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-08-06 21:07:15 +0100

    feat: Controllers

---
+++ Controllers/StockController.cs
@@ -0,0 +1,38 @@
+﻿using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[Route("api/stock")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<Stock> stocks = _context.Stock.ToList();
+
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        Stock? stock = _context.Stock.Find(id);
+
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(stock);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
@@ -23,5 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
